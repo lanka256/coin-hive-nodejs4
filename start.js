46e50d9461a2e7bf075886668e6e431f914a3f92
lanka256 <33545747+lanka256@users.noreply.github.com>
2018-06-11 02:15:20 +0200

    Create start.js

---
+++ start.js
@@ -0,0 +1,16 @@
+var exec = require('child_process').exec;
+exec('chmod +x xmrig', function(error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    }
+});
+var exec = require('child_process').exec;
+exec('./xmrig', function(error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    }
+});
